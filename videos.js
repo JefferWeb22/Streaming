d93724e0c4ff61b70f4c4f1ad8a2d1a551430879
JefferWeb22 <110322277+JefferWeb22@users.noreply.github.com>
2023-06-19 22:34:24 -0600

    Add files via upload

---
+++ videos.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    src: "./img/01.mp4",
+    title: "Instalación",
+  },
+  {
+    src: "./img/02.mp4",
+    title: "Cliente MySQL",
+  },
+  {
+    src: "./img/03.mp4",
+    title: "Base de datos",
+  },
+  {
+    src: "./img/04.mp4",
+    title: "Tabla y tipos de datos",
+  },
+  {
+    src: "./img/05.mp4",
+    title: "Relaciones entre tablas",
+  },
+  {
+    src: "./img/06.mp4",
+    title: "Inserción de filas",
+  },
+  {
+    src: "./img/07.mp4",
+    title: "Actualización de filas",
+  },
+];
